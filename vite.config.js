4b63ac0ac3d292da3f4459e420b227bdd374121d
IzabelleSouza <izabellepsouzaa@gmail.com>
2025-04-13 03:18:15 -0300

    Ajustes no vite.config.js para ambientes local e produção

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    root: '.', // Garante que a raiz seja reconhecida (opcional, pois já é o padrão)
-});
+    base: mode === 'production' ? '/brainstorming-for-class-/' : '/',
+}));
