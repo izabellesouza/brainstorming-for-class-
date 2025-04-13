d13fddebc11f444b7c5b39af60643b1f81016331
IzabelleSouza <izabellepsouzaa@gmail.com>
2025-04-13 06:12:36 -0300

    Corrige paths para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/brainstorming-for-class-/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/brainstorming-for-class-/',  // mantém como estava
+    base: process.env.NODE_ENV === 'production' ? '/brainstorming-for-class-/' : '/',
     plugins: [react()],
 });
